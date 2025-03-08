d2ec79d84ab7a0e75116c6c73299d2ca2fe9b41a
arpondark <arponarpon007@gmail.com>
2025-03-08 14:57:11 +0600

    m.1

--- sorting/Merge.cpp
+++ sorting/Merge.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
 
-// Function to merge two sorted halves of an array
 void merge(int arr[], int left, int mid, int right) {
     int n1 = mid - left + 1;
     int n2 = right - mid;
 
-    // Create temporary arrays
-    int L[n1], R[n2];
+    int* L = new int[n1];
+    int* R = new int[n2];
 
-    // Copy data to temporary arrays
     for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (int i = 0; i < n2; i++)
         R[i] = arr[mid + 1 + i];
 
-    // Merge the temporary arrays back into arr[]
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
@@ -28,43 +25,39 @@ void merge(int arr[], int left, int mid, int right) {
         k++;
     }
 
-    // Copy remaining elements of L[], if any
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    // Copy remaining elements of R[], if any
     while (j < n2) {
         arr[k] = R[j];
         j++;
         k++;
     }
+
+    delete[] L;
+    delete[] R;
 }
 
-// Function to implement merge sort
 void mergeSort(int arr[], int left, int right) {
     if (left < right) {
         int mid = left + (right - left) / 2;
 
-        // Recursively sort first and second halves
         mergeSort(arr, left, mid);
         mergeSort(arr, mid + 1, right);
 
-        // Merge sorted halves
         merge(arr, left, mid, right);
     }
 }
 
-// Function to print an array
 void printArray(int arr[], int size) {
     for (int i = 0; i < size; i++)
         cout << arr[i] << " ";
     cout << endl;
 }
 
-// Main function
 int main() {
     int arr[] = {12, 11, 13, 5, 6, 7};
     int size = sizeof(arr) / sizeof(arr[0]);
