980ef037e84e89854930f089b8af6280979e9216
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-06-11 22:39:46 +0600

    3

--- Homework 1/Queue/2.cpp
+++ Homework 1/Queue/2.cpp
@@ -1,8 +1,54 @@
-/*
+/* LeetCode 1700 - Number of Students Unable to Eat Lunch 
 class Solution {
 public:
     int countStudents(vector<int>& students, vector<int>& sandwiches) {
-        
+        vector<int> count(2);
+        for (int student : students) {
+            count[student]++;
+        }
+        for (int sandwich : sandwiches) {
+            if (count[sandwich] == 0) {
+                return count[0] + count[1];
+            }
+            count[sandwich]--;
+        }
+        return 0;
     }
 };
 */
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int countStudents(vector<int>& students, vector<int>& sandwiches) {
+    vector<int> count(2);  // count[0] for circular, count[1] for square
+
+    // Count student preferences
+    for (int student : students) {
+        count[student]++;
+    }
+
+    // Iterate over sandwiches
+    for (int sandwich : sandwiches) {
+        if (count[sandwich] == 0) {
+            // No students left who want this type
+            return count[0] + count[1];
+        }
+        count[sandwich]--;
+    }
+
+    // All students are able to eat
+    return 0;
+}
+
+int main() {
+    // Sample input
+    vector<int> students = {1,1,1,0,0,1};
+    vector<int> sandwiches = {1,0,0,0,1,1};
+
+    int result = countStudents(students, sandwiches);
+    cout << "Number of students who can't eat: " << result << endl;
+
+    return 0;
+}
---
+++ Homework 1/Queue/3.cpp
@@ -0,0 +1,43 @@
+/*  //*LeetCode Problem: 2073. Time Needed to Buy Tickets //*
+class Solution {
+ public:
+  int timeRequiredToBuy(vector<int>& tickets, int k) {
+    int ans = 0;
+
+    for (int i = 0; i < tickets.size(); ++i)
+      if (i <= k)
+        ans += min(tickets[i], tickets[k]);
+      else
+        ans += min(tickets[i], tickets[k] - 1);
+
+    return ans;
+  }
+};
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int timeRequiredToBuy(vector<int>& tickets, int k) {
+    int time = 0;
+    for (int i = 0; i < tickets.size(); ++i) {
+        if (i <= k)
+            time += min(tickets[i], tickets[k]);
+        else
+            time += min(tickets[i], tickets[k] - 1);
+    }
+    return time;
+}
+
+int main() {
+    // Example 1
+    vector<int> tickets1 = {2, 3, 2};
+    int k1 = 2;
+    cout << "Time for person " << k1 << " to buy tickets: " << timeRequiredToBuy(tickets1, k1) << endl;
+
+    // Example 2
+    vector<int> tickets2 = {5, 1, 1, 1};
+    int k2 = 0;
+    cout << "Time for person " << k2 << " to buy tickets: " << timeRequiredToBuy(tickets2, k2) << endl;
+
+    return 0;
+}
